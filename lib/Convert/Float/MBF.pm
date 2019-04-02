73160ed12723e0fcc5d7130c48d8f6db79a79670
William Scherz <wscherz@suddenlink.net>
2019-04-01 20:27:09 -0500

    initial commit

---
+++ lib/Convert/Float/MBF.pm
@@ -0,0 +1,23 @@
+use strict;
+use warnings;
+
+package Convert::Float::MBF;
+
+use Exporter;
+
+our @ISA = qw(Exporter);
+our @EXPORT = ();
+our %EXPORT_TAGS = ();
+our @EXPORT_OK = ();
+
+# ABSTRACT: whatever
+
+=head1 SYNOPSIS
+
+=cut
+
+=head1 DESCRIPTION
+
+=cut
+
+1;
